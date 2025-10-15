e21dd94db817ab721becef24ff6542c04b4cee19
Ahmed Al Darabee <seahmednail@gmail.com>
2025-10-15 16:55:48 +0300

    uploading-project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+function App() {
+
+    return (
+      <div className='w-full h-screen flex items-center justify-center'>
+          <h3 className='capitalize text-2xl font-bold text-black'>ahmed</h3>
+      </div>
+    )
+}
+
+export default App
---
+++ src/components/Overall.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+const Overall = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Overall
