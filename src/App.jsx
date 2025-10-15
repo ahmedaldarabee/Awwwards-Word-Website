e8454c533042a8f9e324954bc1e75df28fbc46c3
Ahmed Al Darabee <seahmednail@gmail.com>
2025-10-15 17:28:21 +0300

    add  project assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 function App() {
 
     return (
-      <div className='w-full h-screen flex items-center justify-center'>
-          <h3 className='capitalize text-2xl font-bold text-black'>ahmed</h3>
-      </div>
+      <></>
     )
 }
 
